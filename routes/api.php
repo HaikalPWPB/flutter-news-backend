c73f3c6998b6068a3b54761b5791ade78129d230
HaikalPWPB <haikalg2003@gmail.com>
2021-07-01 09:27:33 +0700

    Laravel Passport

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\NewsController;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/news', [NewsController::class, 'getAllNews']);
-Route::get('/news/{id}', [Newscontroller::class, 'getDetailNews']);
-Route::get('/news/search', [NewsController::class, 'search']);
+Route::group(['prefix' => 'v1'], function() {
+    Route::post('/login', [UserController::class, 'login']);
+    Route::post('/register', [UserController::class, 'register']);
+
+    Route::middleware(['auth:api'])->group(function () {        
+        Route::get('/logout', [UserController::class, 'logout']);
+        Route::get('/profile', [UserController::class, 'profile']);
+    
+        Route::get('/news', [NewsController::class, 'getAllNews']);
+        Route::get('/news/{id}', [Newscontroller::class, 'getDetailNews']);
+        Route::get('/news/search', [NewsController::class, 'search']);
+    });
+});
+
